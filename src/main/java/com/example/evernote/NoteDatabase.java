27d3994676336e36c737c0361b7a4b9ea526a337
CsAhmed2020 <csahmed.98.20@gmail.com>
2021-10-16 13:39:48 +0200

    firc

---
+++ src/main/java/com/example/evernote/NoteDatabase.java
@@ -0,0 +1,59 @@
+package com.example.evernote;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Note.class}, version = 1,exportSchema =false)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    //synchronized mean one thread at a time can access this method.
+    // we not use instance = new NoteDatabase as this class is abstract.
+    /*fallbackToDestructiveMigration Allows Room to destructively recreate database tables
+    if Migrations that would migrate old database schemas to the latest schema version are not found.*/
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    //RoomDatabase.Callback To pre-populate a Room database.
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
+
+    }
+}
